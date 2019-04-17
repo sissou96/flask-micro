4599f14df2d51924a8ee1384843269d135fc7da4
Fukuro <sissou96@gmail.com>
2019-04-17 12:36:46 +0200

    Finished part 1: functional deployment on AWS EC2 Micro instances, functional DB, first UI part with a Jinja template, NGINX reverse proxy and Gunicorn server

---
+++ services/users/project/tests/test_users.py
@@ -0,0 +1,168 @@
+import json
+import unittest
+
+from project.tests.base import BaseTestCase
+from project import db
+from project.api.models import User
+
+def add_user(username, email):
+    user = User(username=username, email=email)
+    db.session.add(user)
+    db.session.commit()
+    return user
+
+class TestUserService(BaseTestCase):
+    """Tests for the Users Service"""
+
+    def test_users(self):
+        """Ensure the /ping route behaves correctly"""
+        response = self.client.get('/users/ping')
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('pong!', data['message'])
+        self.assertIn('success', data['status'])
+    
+    def test_add_user(self):
+        """Ensure a new user can be added to the database"""
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'arthur',
+                    'email': 'arthur.carchi@gmail.com'
+                }),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('arthur.carchi@gmail.com was added!', data['message'])
+            self.assertIn('success', data['status'])
+    
+    def test_add_user_invalid_json(self):
+        """
+        Ensure error is thrown if the JSON object does not have a username key.
+        """
+        with self.client:
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'email': 'arthur.carchi@gmail.com'
+                }),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Invalid payload.', data['message'])
+            self.assertIn('fail', data['status'])
+    
+    def test_add_user_duplicate_email(self):
+        """Ensure error is thrown if the email already exists."""
+        with self.client:
+            self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'arthur',
+                    'email': 'arthur.carchi@gmail.com'
+                }),
+                content_type='application/json'
+            )
+            response = self.client.post(
+                '/users',
+                data=json.dumps({
+                    'username': 'arthur',
+                    'email': 'arthur.carchi@gmail.com'
+                }),
+                content_type='application/json'
+            )
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 400)
+            self.assertIn('Sorry. That email already exists.', data['message'])
+            self.assertIn('fail', data['status'])
+    
+    def test_single_user(self):
+        """Ensure get single user behaves correctly."""
+        user = add_user('arthur', 'arthur.carchi@gmail.com')
+        with self.client:
+            response = self.client.get(f'/users/{user.id}')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('arthur', data['data']['username'])
+            self.assertIn('arthur.carchi@gmail.com', data['data']['email'])
+            self.assertIn('success', data['status'])
+
+    def test_single_user_no_id(self):
+        """Ensure error is thrown if an id is not provided."""
+        with self.client:
+            response = self.client.get('/users/blah')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+        
+    def test_single_user_wrong_id(self):
+        """Ensure error is thrown if the id does not exist."""
+        with self.client:
+            response = self.client.get('/users/999')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 404)
+            self.assertIn('User does not exist', data['message'])
+            self.assertIn('fail', data['status'])
+    
+    def test_all_users(self):
+        """Ensure get all users behave correctly."""
+        add_user('michael', 'michael@mherman.org')
+        add_user('arthur', 'arthur.carchi@gmail.com')
+        with self.client:
+            response = self.client.get('/users')
+            data = json.loads(response.data.decode())
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(len(data['data']['users']), 2)
+            self.assertIn('michael', data['data']['users'][0]['username'])
+            self.assertIn('arthur', data['data']['users'][1]['username'])
+            self.assertIn(
+                'michael@mherman.org', data['data']['users'][0]['email']
+            )
+            self.assertIn(
+                'arthur.carchi@gmail.com', data['data']['users'][1]['email']
+            )
+            self.assertIn('success', data['status'])
+    
+    def test_main_no_users(self):
+        """Ensure the main route behaves correctly when no users have been
+        added to the database."""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'All Users', response.data)
+        self.assertIn(b'<p>No users!</p>', response.data)
+    
+    def test_main_with_users(self):
+        """Ensure the main route behaves correctly when users have been
+        added to the database."""
+        add_user('arthur', 'arthur.carchi@gmail.com')
+        add_user('gilou', 'gilou@gmail.com')
+        with self.client:
+            response = self.client.get('/')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'All Users', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'arthur', response.data)
+            self.assertIn(b'gilou', response.data)
+
+    def test_maid_add_user(self):
+        """
+        Ensure a new user can be added to the database via a POST request
+        """
+        with self.client:
+            response = self.client.post(
+                '/',
+                data=dict(username='arthur', email='arthur.carchi@gmail.com'),
+                follow_redirects=True
+            )
+            self.assertEqual(response.status_code, 200)
+            self.assertIn(b'All Users', response.data)
+            self.assertNotIn(b'<p>No users!</p>', response.data)
+            self.assertIn(b'arthur', response.data)
+
+
+if __name__ == '__main__':
+    unittest.main()
