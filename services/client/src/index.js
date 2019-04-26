530f84eadcd8ad2e553a939c76da90f2fc10647b
Fukuro <sissou96@gmail.com>
2019-04-26 13:34:43 +0200

    Added deployment and CI for client service

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios'
+import axios from 'axios';
 
-import UsersList from './components/UsersList'
+import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 class App extends React.Component {
   state = {
-    users: []
+    users: [],
+    username: '',
+    email: '',
+  }
+
+  constructor() {
+    super();
+    this.addUser = this.addUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
@@ -19,6 +28,26 @@ class App extends React.Component {
       .catch((err) => { console.error(err); })
   }
 
+  addUser(event) {
+    event.preventDefault();
+    const data = {
+      username: this.state.username,
+      email: this.state.email
+    }
+    axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+      .then((res) => {
+        this.getUsers();
+        this.setState({ username: '', email: '' })
+      })
+      .catch((err) => { console.error(err); })
+  }
+
+  handleChange(event) {
+    const obj = {}
+    obj[event.target.name] = event.target.value;
+    this.setState(obj);
+  }
+
   render() {
     return (
       <section className="section">
@@ -28,6 +57,13 @@ class App extends React.Component {
               <br/>
               <h1 className="title is-1">All Users</h1>
               <hr/><br/>
+              <AddUser
+                username={this.state.username}
+                email={this.state.email}
+                addUser={this.addUser}
+                handleChange={this.handleChange}
+              />
+              <br/><br/>
               <UsersList users={this.state.users} />
             </div>
           </div>
